1a24cb372734512980ee0fd3167977d1701e0879
youngjun <youngjun@192.168.0.43>
2016-08-22 19:25:45 +0900

    init

---
+++ jwp-fc/src/main/java/next/JwpFcApplication.java
@@ -0,0 +1,12 @@
+package next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpFcApplication.class, args);
+	}
+}
